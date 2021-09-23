6e07d8fd69080955fff433cef75f1b2b046bf77d
DAk001 <def.fed2006@gmail.com>
2021-09-23 15:14:57 +0300

    Added Ellipse

--- lib/figures.hpp
+++ lib/figures.hpp
@@ -2,6 +2,8 @@
 #define _FIGURES_LIB
 
 #include "screen.hpp"
+#include <math.h>
+#define PI 3.14159
 
 namespace figures {
     class Figure {
@@ -22,7 +24,7 @@ namespace figures {
         public:
             Rectangle() {}
 
-            Rectangle(COORD _size) { size = _size; }
+            Rectangle(COORD _size) { setSize(_size); }
 
             void draw(Screen* screen, COORD cameraPOS) {
                 short toX = position.X + size.X - cameraPOS.X;
@@ -30,7 +32,46 @@ namespace figures {
 
                 for (short x = position.X; x <= toX; ++x) {
                     for (short y = position.Y; y <= toY; ++y) 
-                        screen->setPX_s({x, y}, L'V');
+                        screen->setPX_s({x, y}, L'X');
+                }
+            }
+
+            void setSize(COORD _size) { size = _size; }
+            COORD getSize() { return size; }
+        
+        private:
+            COORD size = {0, 0};
+    };
+
+    class Ellipse : public Figure {
+        public:
+            Ellipse() {}
+
+            Ellipse(COORD _size) { setSize(_size); }
+
+            // not the best ever realisation 
+            void draw(Screen* screen, COORD cameraPOS) {
+                short rX = size.X / 2, rY = size.Y / 2;                     // x radius, y radius 
+                short centerX = position.X + rX - cameraPOS.X;
+                short centerY = position.Y + rY - cameraPOS.Y;
+
+                double aS = PI / size.X / size.Y, aT;                       // step angle
+                const short rX_s = rX * rX, rY_s = rY * rY, rXY = rX * rY;  // x radius sqare, y radius sqare, x radius * y radius
+                short toX, toY;
+                short x, y;
+
+                for (double a = 0; a <= PI; a += aS) {
+                    aT = tan(a);
+                    toY = rXY / sqrt(rX_s + rY_s * aT * aT);                // ellipse formula
+                    toX = toY * aT; 
+
+                    y = centerY - toY;
+                    for (x = centerX - toX; x < centerX + toX; ++x)         // top half of the ellipse
+                        screen->setPX_s({x, y}, L'X');
+                    
+                    y = centerY + toY - 1;
+                    for (x = centerX - toX; x < centerX + toX; ++x)         // bottom half of the ellipse
+                        screen->setPX_s({x, y}, L'X');
                 }
             }
 
