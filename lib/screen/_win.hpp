6865cf70d8a82fd3cf048a77011f51bb895d695b
DAk001 <def.fed2006@gmail.com>
2021-09-30 20:07:45 +0300

    Update WinScreen 🖥
    - Screen resizes automaticly
    - FPS limit added
    - FPS tracker added
    - Optimised

--- lib/screen/_win.hpp
+++ lib/screen/_win.hpp
@@ -24,9 +24,6 @@ class WinScreen {
             delete [] screen;
         }
 
-        // resizing automaticly screen if needed
-        bool autoSize = false;
-
         // unsafe setPX ; better use setPX_s
         inline void setPX(COORD coord, wchar_t c);
         // safe setPX
@@ -39,7 +36,7 @@ class WinScreen {
         // draw screen
         void draw(bool autoClear);
         // clear screen
-        void clear();
+        inline void clear();
         // return screen buffer
         const wchar_t* getScreen();
 
@@ -52,17 +49,35 @@ class WinScreen {
         // sets font
         void setFont(COORD size);
 
+        void setFpsLimit(int fps) { frameTime = 1.0f / fps / 0.9 * 1000; }
+
+        // current fps
+        int fps = 0;
+
+        // toggle resizing automaticly 
+        void autosizeToggle(bool state) { autoSize = state; }
+
+        // toggle metadata
+        void metaToggle(bool state) { showMeta = state; }
+
     private:
         HANDLE sHandler;
         CONSOLE_SCREEN_BUFFER_INFO screenInfo;
         COORD scrSize;
         size_t pixelC;
+        bool showMeta = false;
+        bool autoSize = false;
 
         wchar_t* screen;
         DWORD dWritten;
 
         void setupScreen();
         void resizeScreen();
+
+        // time
+        int frameTime = 0, avgWait = 0;  // in milliseconds
+        std::chrono::system_clock::time_point t1 = std::chrono::system_clock::now(), t2 = std::chrono::system_clock::now();
+        std::chrono::duration<double> tdiff;
 };
 
 void WinScreen::setPX(COORD coord, wchar_t c) {
@@ -75,9 +90,41 @@ void WinScreen::setPX_s(COORD coord, wchar_t c) {
     };
 }
 
-void WinScreen::draw(bool autoClear = true) {
-    if (autoSize) resizeScreen();
+void WinScreen::draw(bool autoClear = true) {    
+    // FIXME: fails to get correct time 
+    // calculate frame time
+    t2 = std::chrono::system_clock::now();
+    tdiff = t2 - t1;
+    t1 = t2;
+    double curr_tdiff = tdiff.count();
+
+    // FIXME: Sleep function not working as expected 
+    if (frameTime) {
+        int waitTime = frameTime - curr_tdiff * 1000;
+        avgWait += waitTime;
+        if (avgWait > 0) {
+            Sleep(avgWait);
+        }
+    }
+    
+    // displays metadata
+    if (showMeta) {
+        // average framerate
+        int frame = curr_tdiff > 0 ? 1.0f / curr_tdiff : fps;
+        fps = fps - fps / 50 + frame / 50;
+
+        std::string metadata = std::string() + "[" + std::to_string(fps) + "]";
+        const char* cmetadata = metadata.c_str(); 
+
+        short min = scrSize.X > metadata.size() ? metadata.size() : scrSize.X;
+        short shift = scrSize.X - min;
+        for (short m = 0; m < min; ++m) {
+            screen[m + shift] = (wchar_t) cmetadata[m];
+        }
+    }
+
     WriteConsoleOutputCharacterW(sHandler, screen, pixelC, {0, 0}, &dWritten);
+    if (autoSize) resizeScreen();
     if (autoClear) clear();
 }
 
@@ -86,10 +133,11 @@ void WinScreen::resizeScreen() {
     if (screenInfo.dwSize.X == scrSize.X && screenInfo.dwSize.Y == scrSize.Y) return;
     scrSize = screenInfo.dwSize;
     pixelC = scrSize.X * scrSize.Y;
+    setupScreen();
 }
 
 void WinScreen::clear() {
-    std::fill_n(screen, pixelC, L' ');
+    for(int p = 0; p < pixelC; ++p) screen[p] = L' ';
 }
 
 const wchar_t* WinScreen::getScreen() {
@@ -110,13 +158,5 @@ void WinScreen::setFont(COORD size) {
 void WinScreen::setupScreen() {
     screen = new wchar_t[pixelC]();
 }
-
-// void WinScreen::initConsoleColors() {
-//     HKEY hKey;
-//     long nError = RegOpenKeyExA(HKEY_CURRENT_USER, "Console", 0, KEY_WRITE, &hKey)
-
-//     DWORD color = 252;
-//     RegSetValueExA(hKey, "ScreenColors", 0, REG_DWORD, &color , sizeof(color))
-// }
 
 #endif
