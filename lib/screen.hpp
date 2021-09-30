6295b2d33f01eff7c93072889d2644cb740d07cd
DAk001 <def.fed2006@gmail.com>
2021-09-30 20:08:03 +0300

    Added ::keys

--- lib/screen.hpp
+++ lib/screen.hpp
@@ -4,9 +4,18 @@
     #ifdef _WIN32
     #include "screen/_win.hpp"
     using Screen = WinScreen;
-    #endif
 
-    #ifdef linux
+    namespace keys {
+        bool down(char key) {
+            return -GetAsyncKeyState(key) == 0x8000;
+        }
+
+        bool up(char key) {
+            return -GetAsyncKeyState(key) == 0x7FFF;
+        }
+    }
+
+    #else 
     #include "screen/_unix.hpp"
     using Screen = UnixScreen;
     #endif
