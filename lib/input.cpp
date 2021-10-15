99733536ee893e703ca563f490c8182715023676
DAk001 <def.fed2006@gmail.com>
2021-10-15 23:24:18 +0300

    For future implementation

---
+++ lib/input.cpp
@@ -0,0 +1,5 @@
+#include "../congl.h"
+
+namespace ConGL {
+    // TODO: Input implementation
+}
---
+++ lib/input_unix.cpp
@@ -0,0 +1,5 @@
+#include "../congl.h"
+
+namespace ConGL {
+    // TODO: UnixInput implementation
+}
