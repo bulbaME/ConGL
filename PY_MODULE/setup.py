d9be94b69af0461aea7b295ba39e8d2a1ca437a3
DAk001 <def.fed2006@gmail.com>
2021-11-20 20:25:15 +0200

    Update PY_MODULE

--- PY_MODULE/setup.py
+++ PY_MODULE/setup.py
@@ -1,24 +1,28 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
+
 setup(
-  name = 'ConGL',         
-  packages = ['ConGL'],  
-  version = '0.1',      
-  license='MIT',        
-  description = 'Console Graphics Library',  
-  author = 'bulba',
-  author_email= 'rily.ylir1@gmail.com',
-  url = 'https://github.com/bulbaME/ConGL/PY_MODULE', 
-  download_url = 'https://github.com/bulbaME/ConGL/releases/download/ConGL/ConGL-0.1.tar.gz', 
-  keywords = ['console', 'cmd', 'engine', 'congl', 'terminal'], 
-  install_requires=[ ],
-  classifiers=[
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10'
-  ],
+    name = 'ConGL',
+    packages = ['ConGL'],
+    version = '0.1',
+    license = 'MIT',
+    platforms = ['win'],
+    description = 'Console Graphics Library',
+    author = 'bulba',
+    author_email= 'rily.ylir1@gmail.com',
+    url = 'https://github.com/bulbaME/ConGL/PY_MODULE',
+    download_url = '',
+    keywords = ['console', 'cmd', 'engine', 'congl', 'terminal'],
+    package_dir = { 'ConGL': 'ConGL'}, 
+	py_modules = ['ConGL/docs'],
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
+    ]
 )
